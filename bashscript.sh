58e3bc213be77230cb4d9eb656720c62f4abf8cb
ayaashraf90 <108239881+ayaashraf90@users.noreply.github.com>
2022-06-29 17:14:18 +0200

    Update bashscript.sh

--- bashscript.sh
+++ bashscript.sh
@@ -6,12 +6,8 @@ echo "$data"
 echo "**********************************************************"
 number=`cut -f 3 -d " " data.txt`
 readarray -t email <<<`cut -f 2 -d " " data.txt`
-#readarray -t arr < data.txt
-#echo "${email[@]}"
 echo "2- The IDs number type are:- "
 echo "----------------------------"
-#for j in "${arr[@]}"; do
-#email=`echo $j |cut -f 2 -d " "`
 for i in $number 
 do
 if [ $(($i%2)) -eq 0 ]; then
@@ -20,9 +16,7 @@ else
 echo the ID $i  is odd
 fi
 done
-#done
 echo "**********************************************************"
-#echo $data|grep -E '\.' 
-echo "3- The FQDN email addresses are:- "
+ echo "3- The FQDN email addresses are:- "
 echo "----------------------------"
 echo "`grep -E '\.'  data.txt | grep -E '@'` "
